8cf27a53e969828ae0cf30a3a45f87febb311d1b
Meena2098 <42998167+Meena2098@users.noreply.github.com>
2018-09-14 14:47:59 +0530

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    long long a;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &a);
+
+    while(a != 0)
+    {
+        // a = a/10
+        a /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
